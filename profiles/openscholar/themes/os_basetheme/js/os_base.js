8fc2575565383ccd9185cc5f2422912efcd98b99
francoisdm <francois.diazmaurin@gmail.com>
2014-09-24 13:56:03 +0200

    openscholar push

---
+++ profiles/openscholar/themes/os_basetheme/js/os_base.js
@@ -0,0 +1,27 @@
+
+// Fixes tab behavior after using the skip link in IE or Chrome
+Drupal.behaviors.osBase_skipLinkFocus = {
+  attach: function (ctx) {
+    var $ = jQuery,
+        $skip = $('#skip-link a', ctx);
+    
+    $skip.click(function (e) {
+      var target = $skip[0].hash.replace('#', '');
+
+      setTimeout (function () {
+        $('a[name="'+target+'"]').attr('tabindex', -1).focus();
+      }, 100);
+    });
+  }
+};
+
+
+
+jQuery(document).ready(function() {
+jQuery('.sv-list-image.sv-list-view-teaser.sv-list-layout-grid .file.file-image, .sv-list-video.sv-list-view-teaser.sv-list-layout-grid .file.file-video').hover(function() {
+    jQuery(this).find('.field-name-os-file-description').slideToggle();
+	});
+});
+
+
+
